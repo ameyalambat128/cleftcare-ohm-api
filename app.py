1c5a3653b9f4f020d71ae3441d3248d92f6cc0a7
Ameya Lambat <63185829+ameyalambat128@users.noreply.github.com>
2024-11-25 05:30:32 -0700

    feat: updated FastAPI server
    Predict Endpoint tested and working locally

--- app.py
+++ app.py
@@ -24,19 +24,19 @@ s3 = boto3.client(
 )
 
 
-# Health Check
+"""
+Health Check Endpoint
+"""
+
+
 @app.get("/")
 async def health_check():
     return {"status": "ok"}
-
-# Request Model
 
 
 class PredictRequest(BaseModel):
     userId: str
     uploadFileName: str
-
-# GET Endpoint
 
 
 @app.get("/predict-test")
@@ -65,7 +65,7 @@ async def predict_ohm():
 
         # Process the downloaded file for prediction
         temp_folder = Path(temp_file_path).parent
-        perceptual_rating = predict_ohm_rating_old(temp_folder)
+        perceptual_rating = predict_ohm_rating(temp_folder)
 
         return {"perceptualRating": perceptual_rating}
 
@@ -74,32 +74,47 @@ async def predict_ohm():
             status_code=500, detail=f"Error processing file: {str(e)}")
 
 
-# POST Endpoint
+"""
+Predict Endpoint:
+- Accepts a POST request with userId and uploadFileName
+"""
 
 
 @app.post("/predict")
 async def predict_ohm(request: PredictRequest):
     user_id = request.userId
     upload_file_name = request.uploadFileName
-
+    print(
+        f"Received request for userId: {user_id}, uploadFileName: {upload_file_name}")
     # Generate S3 file path based on userId
-    s3_key = f"{upload_file_name}.m4a"  # Customize if needed
+    s3_key = upload_file_name  # Customize if needed
 
     try:
+        audio_dir = os.path.join(os.getcwd(), "audios")
+        os.makedirs(audio_dir, exist_ok=True)
         # Download file from S3
-        with NamedTemporaryFile(delete=False, suffix=".m4a") as temp_file:
+        with NamedTemporaryFile(delete=True, suffix=".m4a", dir=audio_dir) as temp_file:
             temp_file_path = temp_file.name
             s3.download_file(S3_BUCKET_NAME, s3_key, temp_file_path)
+            print("Downloaded file from S3: ", temp_file_path)
 
-        # Convert .m4a to .wav
-        wav_path = temp_file_path.replace(".m4a", ".wav")
-        os.system(f"ffmpeg -i {temp_file_path} {wav_path}")
+            # Check if the file is already in .wav format
+            if not temp_file_path.endswith(".wav"):
+                # Convert .m4a to .wav
+                wav_path = temp_file_path.replace(".m4a", ".wav")
+                os.system(f"ffmpeg -i {temp_file_path} {wav_path}")
+            else:
+                wav_path = temp_file_path
 
         # Process the downloaded file for prediction
         temp_folder = Path(temp_file_path).parent
-        perceptual_rating = test_predict_ohm_rating(temp_folder)
-
-        return {"userId": user_id, "perceptualRating": perceptual_rating}
+        perceptual_rating = predict_ohm_rating(temp_folder)
+        print(f"Predicted perceptual rating: {perceptual_rating}")
+        # Delete the .wav file if it was created
+        if os.path.exists(wav_path) and wav_path != temp_file_path:
+            os.remove(wav_path)
+            print(f"Deleted .wav file: {wav_path}")
+        return {"perceptualRating": perceptual_rating}
 
     except Exception as e:
         raise HTTPException(
